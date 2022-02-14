6dacc73875b479e56366a58fda787ef00e0d6ed4
Ronada <basharonada@gmail.com>
2022-02-14 15:08:16 +0100

    PokemonsList component created

---
+++ src/components/PokemonsList/PokemonsList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function PokemonsList():JSX.Element {
+    return (
+      <div className="items-wrapper">
+        <div className="items-title">
+          <h1>All Pokemons</h1>
+        </div>
+        Pokemons list here ...
+      </div>
+    );
+  }
+  
