b28871d2c03a19bae82289c213b0e13650d965e7
Ronada <basharonada@gmail.com>
2022-02-14 18:57:20 +0100

    Pokemon details page - api connection

--- src/components/PokemonItem/PokemonItem.tsx
+++ src/components/PokemonItem/PokemonItem.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Pokemon from '../../models/Pokemon';
 import pokemonImg from '../../pokemon.png';
 import "./PokemonItem.scss"
 
 export const PokemonItem:React.FC<{item: Pokemon}> = (props) => {
-    return (
-      <div className="item">
+  const navigate = useNavigate();
+  function goToDetailsPage(){
+    navigate(`pokemons/${props.item.id}`);
+  }
+  return (
+    <div className="item" onClick={goToDetailsPage}>
         <img src={props.item.sprites.front_default ? props.item.sprites.front_default : ""} alt="img" />
         <h2>{props.item?.name}</h2>
       </div>
