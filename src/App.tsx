85b09ca37a1c275dc3663f399e0aab60809ce529
Ronada <basharonada@gmail.com>
2022-02-14 17:50:21 +0100

    App routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import PokemonsList from './components/PokemonsList/PokemonsList';
+import { Route, Routes } from 'react-router-dom';
+import PokemonDetails from './components/PokemonDetails/PokemonDetails';
 
 function App() {
   return (
     <div className="App">
-      <PokemonsList />
+      <Routes>
+        <Route path="/" element={<PokemonsList />} />
+        <Route path="/pokemons/:id" element={<PokemonDetails />} />
+      </Routes>
     </div>
   );
 }
